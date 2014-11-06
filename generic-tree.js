568af9bc07243443cec3278fe70f9c6d3b56279b
Arnelle Balane <arnellebalane@gmail.com>
2014-11-07 01:08:32 +0800

    implemented delete method

--- generic-tree.js
+++ generic-tree.js
@@ -9,22 +9,37 @@
         this.root = null;
 
         this.insert = function(key, parent, properties) {
-            parent = parent && parent instanceof Node ? [parent] : this.search(parent);
+            parent = parent !== undefined && parent instanceof Node ? [parent] : this.search(parent);
             var node = new Node(key, properties);
             if (parent === null && !this.root) {
                 this.root = node;
             } else if (parent === null && !!this.root) {
-                throw new Error('Generic Tree already has a root. Please specify the node\'s parent.');
+                throw new Error('GenericTree already has a root. Please specify the node\'s parent.');
             } else if (!parent.length) {
-                throw new Error('Node\'s parent not found.');
+                throw new Error('Parent node not found.');
             } else {
                 parent[0].insert(node);
             } 
             return node;
         };
 
+        this.delete = function(node) {
+            var targets = node instanceof Node ? [node] : this.search(node);
+            if (targets === null || !targets.length) {
+                throw new Error('Target node not found.');
+            }
+            for (var i = 0; i < targets.length; i++) {
+                var target = targets[i];
+                if (target === this.root) {
+                    this.root = null;
+                } else {
+                    target.parent.delete(target);
+                }
+            }
+        };
+
         this.search = function(key) {
-            return key && this.root ? this.root.search(key) : null;
+            return key !== undefined && this.root ? this.root.search(key) : null;
         };
     }
 
@@ -41,6 +56,10 @@
         this.insert = function(child) {
             this.children.push(child);
             child.parent = this;
+        };
+
+        this.delete = function(child) {
+            this.children.splice(this.children.indexOf(child), 1);
         };
 
         this.search = function(key) {
