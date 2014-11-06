a6cc0970f28cb323c7804ce7366d9ddd56acb56b
Arnelle Balane <arnellebalane@gmail.com>
2014-11-06 23:33:48 +0800

    implemented insert method

--- generic-tree.js
+++ generic-tree.js
@@ -5,18 +5,20 @@
         root.GenericTree = library();
     }
 })(this, function() {
-
-
     function GenericTree() {
         this.root = null;
 
         this.insert = function(key, parent, properties) {
-            parent = parent && parent instanceof Node ? parent : this.search(parent);
-            var node = new Node(key, parent, properties);
-            if (parent === null) {
+            parent = parent && parent instanceof Node ? [parent] : this.search(parent);
+            var node = new Node(key, properties);
+            if (parent === null && !this.root) {
                 this.root = node;
+            } else if (parent === null && !!this.root) {
+                throw new Error('Generic Tree already has a root. Please specify the node\'s parent.');
+            } else if (!parent.length) {
+                throw new Error('Node\'s parent not found.');
             } else {
-                parent.insert(node);
+                parent[0].insert(node);
             } 
             return node;
         };
@@ -26,9 +28,9 @@
         };
     }
 
-    function Node(key, parent, properties) {
+    function Node(key, properties) {
         this.key = key;
-        this.parent = parent;
+        this.parent = null;
         this.children = [];
 
         properties = properties && typeof properties === 'object' ? properties : {};
@@ -38,19 +40,17 @@
 
         this.insert = function(child) {
             this.children.push(child);
+            child.parent = this;
         };
 
         this.search = function(key) {
-            if (this.key === key) {
-                return this;
+            var results = this.key === key ? [this] : [];
+            for (var i in this.children) {
+                results = results.concat(this.children[i].search(key));
             }
-            
+            return results;
         };
     }
-
-    var tree = new GenericTree();
-    var node = tree.insert('/');
-    node = tree.insert('a', node);
 
     return GenericTree;
 });
