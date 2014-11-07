ae314353cc64b2cd6730e38a1263cc2c83cd7859
Arnelle Balane <arnellebalane@gmail.com>
2014-11-07 22:54:12 +0800

    refactored resolving of path and its usage

--- assets/javascripts/virtual-filesystem.js
+++ assets/javascripts/virtual-filesystem.js
@@ -11,18 +11,21 @@
         this.pointer = this.tree.root;
 
         this.mkdir = function(path) {
-            var parent = this._resolve_path(path);
-            var name = path.split('/').pop();
+            path = path.replace(/\/+$/g, '');
+            var segments = path.split('/');
+            var parent = this._resolve_path(segments.slice(0, segments.length - 1).join('/'));
+            var name = segments[segments.length - 1];
             this.tree.insert(name, parent, { type: 'directory' });
         };
 
         this.rmdir = function(path) {
-            path = path.replace(/\/$/g, '');
+            path = path.replace(/\/+$/g, '');
             if (!path.length) {
                 throw new Error('You cannot delete the root directory.');
             }
-            var parent = this._resolve_path(path);
-            var name = path.split('/').pop();
+            var segments = path.split('/');
+            var parent = this._resolve_path(segments.slice(0, segments.length - 1).join('/'));
+            var name = segments[segments.length - 1];
             var node = parent.find(name);
             this.tree.delete(node);
         };
@@ -30,13 +33,13 @@
         this._resolve_path = function(path) {
             path = path.split('/');
             var parent = path[0].length ? this.pointer : this.tree.root;
-            for (var i = !path[0].length ? 1 : 0; i < path.length - 1; i++) {
+            for (var i = !path[0].length ? 1 : 0; i < path.length; i++) {
                 if (path[i] === '..') {
                     if (parent === this.tree.root) {
                         throw new Error('No more directories beyond root directory.');
                     }
                     parent = parent.parent;
-                } else if (path[i] !== '.') {
+                } else if (path[i] !== '.' && path[i].length) {
                     parent = parent.find(path[i]);
                     if (parent === null) {
                         throw new Error('Path does not exists: ' + path.slice(0, i + 1).join('/'));
