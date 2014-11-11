efc487c1efef467d53bf5bb2d2b4a97fe5d073eb
Arnelle Balane <arnellebalane@gmail.com>
2014-11-12 00:58:32 +0800

    implemented logger method

--- assets/javascripts/tree-terminal.js
+++ assets/javascripts/tree-terminal.js
@@ -8,20 +8,30 @@ var terminal = {
             terminal.input.focus();
         });
         terminal.input.on('keydown', terminal.keypressed);
+        terminal.log('Generic Tree Command Line Tool', 'yellow');
+        terminal.log('enter `help` to view available commands', 'yellow');
     },
     keypressed: function(e) {
         if (e.keyCode === 9) {
             e.preventDefault();
+        } else if (e.keyCode === 13) {
+            e.preventDefault();
+            // execute the command in buffer
         } else {
             var length = terminal.input.val().length + terminal.prompt_length;
             length -= e.keyCode === 8 ? 1 : 0;
-            terminal.input.height((~~(length / 87) + 1) * 14);
+            terminal.input.height((~~(length / 86) + 1) * 14);
             if (terminal.contents.height() >= 345) {
                 terminal.contents.addClass('overflown');
             } else {
                 terminal.contents.removeClass('overflown');
             }
         }
+    },
+    log: function(message, color) {
+        message = $('<p>' + message + '</p>');
+        message.addClass(color || '');
+        terminal.prompt.before(message);
     }
 };
 terminal.initialize();
